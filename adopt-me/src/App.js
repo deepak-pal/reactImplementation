374b9741d2b6911f7f516884c598ebbbc04dff2a
deepak-pal <deepak.pal@technia.com>
2020-02-11 01:42:16 +0530

    add parcel and bundle the build process and seprated out the pet commentd

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,10 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me! "),
     React.createElement("h1", {}, "Adopt Me! "),
     React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havnes" }),
     React.createElement(Pet, {
@@ -18,4 +15,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
