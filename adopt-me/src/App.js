5323fcc2e57ef69d329545d3c18090b0b89c17f9
deepak-pal <deepak.pal@technia.com>
2020-02-10 23:51:00 +0530

    Add two comments pet and app

---
+++ adopt-me/src/App.js
@@ -0,0 +1,15 @@
+const Pet = ( {name, animal, breed})  => {
+    return React.createElement("div", {}, [        
+        React.createElement("h1", {},name),
+     React.createElement("h2", {}, animal),
+      React.createElement("h2", {},breed) ]);
+  };
+
+
+
+
+const App = ( )  => {
+    return React.createElement("div", {},[React.createElement("h1", {}, "Adopt Me! "),  React.createElement(Pet, {name:"Luna", animal:"Dog", breed: "Havnes"}),
+     React.createElement(Pet, {name:"Pepper ", animal:"Bird", breed: "Tannes"}) ]);
+  };
+  ReactDOM.render(React.createElement(App), document.getElementById("root"));
